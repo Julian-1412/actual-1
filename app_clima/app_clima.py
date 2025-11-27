ddebf9ad9067828a9c98d5d07ad945c678f09381
Julian-1412 <julian.seg1412@gmail.com>
2025-11-27 12:43:37 -0500

    adding weather app fixed

--- app_clima/app_clima.py
+++ app_clima/app_clima.py
@@ -2,7 +2,7 @@ import requests
 
 city=input("Ingrese el nombre de la ciudad a la que desea consultar el clima: ").capitalize()
 
-url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=7a79dd3d5f62d67d5b7748d255209a2a&units=metric"
+url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=7a79dd3d5f62d67d5b7748d255209a2a&units=metric&lang=es"
 
 response=requests.get(url)
 
@@ -16,4 +16,6 @@ La sensacion termica es de: {data_main['feels_like']:.1f} °C
 La temperatura minima sera de: {data_main['temp_min']:.1f} °C
 La temperatura maxima sera de: {data_main['temp_max']:.1f} °C
 La humedad actual es de: {data_main['humidity']} %
+Descripcion del clima es: {data['weather'][0]['description']}
 """)
+
