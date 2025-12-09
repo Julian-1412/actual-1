9b9e751e28ee3b61d91bf06d0fb83ea2a2fa4d05
Julian Elejalde <julian.seg1412@gmail.com>
2025-12-08 19:49:57 -0500

    add logic exercise

---
+++ ejercicios de clase/clase_logica.py
@@ -0,0 +1,29 @@
+# init= 0
+# nums=[26,7,2,9,5,27,8,12,6]
+# for num in nums:
+#     if num > init:
+#         init = num
+
+# print(init)
+
+# palabra="radar"
+# alreves=""
+# for i in range((len(palabra))-1,-1,-1):
+#     alreves += palabra[i]
+# if palabra == alreves:
+#     print("True")
+# else:
+#     print("False")
+
+# FizzBuzz
+
+# for i in range(1,21):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
